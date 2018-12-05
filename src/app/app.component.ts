bb685d1af034979eb2a7b984a5ab1ebd7ff2940f
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2018-12-06 00:43:57 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+// import TileLayer from 'ol/layer/Tile.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
+import Camiler from './camiler.js'
+import { Icon, Style } from 'ol/style.js'
+import { click, pointerMove, altKeyOnly } from 'ol/events/condition.js';
+import Select from 'ol/interaction/Select.js';
+import Overlay from 'ol/Overlay.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'basaksehir-camiler';
+  @ViewChild('map') map: ElementRef
+  @ViewChild('overlayer') overlayer: ElementRef
+  name
+  capacity
+  ngOnInit() {
+    var map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: this.map.nativeElement,
+      view: new View({
+        projection: 'EPSG:3857',
+        center: [3205929.25726942, 5027538.321153645],
+        zoom: 14
+      }),
+      controls: []
+    })
+
+    const style = new Style({
+      image: new Icon(({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'assets/mosque.png',
+        scale: .5
+      }))
+    });
+    var source = new VectorSource({
+      loader: function (extent, resolution, projection) {
+        source.addFeatures(
+          new GeoJSON().readFeatures(Camiler, { featureProjection: projection }));
+      }
+    });
+    var layer = new VectorLayer({
+      source: source,
+      style
+    })
+    map.addLayer(layer)
+
+    // window.map = map
+    // var selectPointerMove = new Select({
+    //   condition: pointerMove
+    // });
+    // map.addInteraction(selectPointerMove)
+    // selectPointerMove.on('select', function(e,e2){
+    //   console.log('e', e.target)
+    // })
+
+    // var overlay = new Overlay({
+    //   element: this.overlayer.nativeElement,
+    //   autoPan: true,
+    //   autoPanAnimation: {
+    //     duration: 250
+    //   }
+    // });
+    // map.addOverlay(overlay)
+    map.on('pointermove', (browserEvent) => {
+      var coordinate = browserEvent.coordinate;
+      var pixel = browserEvent.pixel;
+      map.forEachFeatureAtPixel(pixel, (feature, layer) => {
+        this.capacity = feature.get('capacity')
+        this.name = feature.get('name')
+      })
+    })
+  }
 }
---
+++ src/app/camiler.js
@@ -0,0 +1,28 @@
+export default {
+    "type": "FeatureCollection",
+    "name": "basaksehir-camiler",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        { "type": "Feature", "properties": { "capacity": 5000, "name": "TUNAHAN CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.809495154952231, 41.109076597965945 ] } },
+        { "type": "Feature", "properties": { "capacity": 2000, "name": "İMAM-I AZAM CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.803977898159523, 41.109123614289906 ] } },
+        { "type": "Feature", "properties": { "capacity": 300, "name": "ÖMER NASUHİ BİLMEN CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.806453123638633, 41.113345151143328 ] } },
+        { "type": "Feature", "properties": { "capacity": 500, "name": "ŞEYH ŞAMİL CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.804932107981834, 41.101022596687159 ] } },
+        { "type": "Feature", "properties": { "capacity": 600, "name": "SÜLEYMAN ÇELEBİ CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.800343060743771, 41.105313223842614 ] } },
+        { "type": "Feature", "properties": { "capacity": 870, "name": "ENSAR CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.804945108115636, 41.096210491402232 ] } },
+        { "type": "Feature", "properties": { "capacity": 300, "name": "MEDİNE-İ MÜDAFİİ FAHRETTİN PAŞA CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.804552504074305, 41.103943780153585 ] } },
+        { "type": "Feature", "properties": { "capacity": 750, "name": "MEVLANA CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.810683367183287, 41.101212644097103 ] } },
+        { "type": "Feature", "properties": { "capacity": 700, "name": "SARI SALTIK CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.810553365845102, 41.095330715795853 ] } },
+        { "type": "Feature", "properties": { "capacity": 500, "name": "HZ SELMAN-İ FARİSİ CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.809908559207681, 41.086363778413656 ] } },
+        { "type": "Feature", "properties": { "capacity": 400, "name": "GÖÇMEN KONUTLARI CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.796458620758688, 41.104588343044028 ] } },
+        { "type": "Feature", "properties": { "capacity": 750, "name": "MEHMET ZAHİT KOTKU CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.79056306007185, 41.100191867532814 ] } },
+        { "type": "Feature", "properties": { "capacity": 500, "name": "HACI AHMET EFENDİ CAMİ:" }, "geometry": { "type": "Point", "coordinates": [ 28.789812952350502, 41.101431099976487 ] } },
+        { "type": "Feature", "properties": { "capacity": 3000, "name": "AHMET YESEVİ CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.786809921438344, 41.104769563993862 ] } },
+        { "type": "Feature", "properties": { "capacity": 400, "name": "MÜDERRİS HALİL HİLMİ CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.784493297591826, 41.107306604780433 ] } },
+        { "type": "Feature", "properties": { "capacity": 800, "name": "Prf. Dr. MAHMUD ESAD ÇOŞAN CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.786282116005303, 41.110446914098532 ] } },
+        { "type": "Feature", "properties": { "capacity": 500, "name": "AKİF İNAN CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.790213356472126, 41.114427427198564 ] } },
+        { "type": "Feature", "properties": { "capacity": 400, "name": "HACI SİMAVİ CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.789902653273849, 41.097500742332464 ] } },
+        { "type": "Feature", "properties": { "capacity": 700, "name": "YUNUS EMRE CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.810116561348803, 41.119145158823748 ] } },
+        { "type": "Feature", "properties": { "capacity": 1750, "name": "ŞEHİT ÖMER HALİS DEMİR CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.771925418222448, 41.094447009505494 ] } },
+        { "type": "Feature", "properties": { "capacity": 500, "name": "HAYRETTİN ATMACA CAMİ" }, "geometry": { "type": "Point", "coordinates": [ 28.78821328588436, 41.095743663163091 ] } }
+    ]
+}
